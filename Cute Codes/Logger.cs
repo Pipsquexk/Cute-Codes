c570319f0124dd537d0673188203616329244577
Liam Swetnam <LHGames21@gmail.com>
2024-03-20 02:24:21 -0700

    Initial commit

---
+++ Cute Codes/Logger.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Cute_Codes
+{
+    internal class Logger
+    {
+        private static Logger instance;
+
+        private static readonly object threadLock = new();
+
+        private static DateTime startTime;
+
+        private static string logPath = "NULL";
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (threadLock)
+                {
+                    instance ??= new Logger();
+                    return instance;
+                }
+            }
+        }
+
+        private bool initialized = false;
+
+        Logger()
+        {
+            Log(ConsoleColor.Blue, "Initializing Logger...");
+            InitLogger();
+        }
+
+        /// <summary>
+        /// Initializes The logger instance to be used in other classes
+        /// </summary>
+        private void InitLogger()
+        {
+            try
+            {
+                if (!Directory.Exists("Logs")) Directory.CreateDirectory("Logs");
+
+                startTime = DateTime.Now;
+                logPath = "Logs/" + startTime.ToString("MM-dd-yyyy HH-mm-ss") + ".log";
+                FileStream logFileStrm = File.Create(logPath);
+                logFileStrm.Close();
+                initialized = true;
+                Log(ConsoleColor.Green, "Successfully initialized Logger!");
+            }
+            catch (Exception ex)
+            {
+                Log(ConsoleColor.Red, $"Logger failed to properly initialize with the exception: {ex}");
+                Log(ConsoleColor.Blue, "Reinitializing Logger...");
+                InitLogger();
+            }
+        }
+
+        /// <summary>
+        /// Logs text to console and the current log file
+        /// </summary>
+        /// <param name="txt">Text to be displayed in the console and written to the log file</param>
+        public void Log(string txt)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            string time = DateTime.Now.ToString("HH:mm:ss ff");
+            Console.Write(time);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("] ");
+
+            Console.Write(txt + "\n");
+
+
+            if (!initialized) return;
+
+            LogToFile($"[{time}] {txt}");
+        }
+
+        /// <summary>
+        /// Logs colored text to console and the current log file
+        /// </summary>
+        /// <param name="txt">Text to be displayed in the console and written to the current log file</param>
+        public void Log(ConsoleColor color, string txt)
+        {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            string time = DateTime.Now.ToString("HH:mm:ss ff");
+            Console.Write(time);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("] ");
+
+            Console.ForegroundColor = color;
+
+            Console.Write(txt + "\n");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+            if (!initialized) return;
+
+            LogToFile($"[{time}] {txt}");
+        }
+
+        /// <summary>
+        /// Logs text to log file
+        /// </summary>
+        /// <param name="txt">Text to be written to the current log file</param>
+        public void LogToFile(string txt)
+        {
+            if(File.Exists(logPath))
+            {
+                StreamWriter writer = File.AppendText(logPath);
+                writer.WriteLine(txt);
+                writer.Close();
+            }
+            else
+            {
+                initialized = false;
+                Log(ConsoleColor.Red, "Logger failed to properly initialize");
+                Log(ConsoleColor.Green, "Reinitializing Logger...");
+                InitLogger();
+            }
+        }
+    }
+}
---
+++ Cute Codes/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Cute_Codes
+{
+    internal class Program
+    {
+
+        private static FileInfo fileToConv;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Logger.Instance.Log(ConsoleColor.Green, "Started Cute Codes!");
+            if (args.Length < 1)
+            {
+                FileInput();
+            }
+            else
+            {
+                fileToConv = new(args[0]);
+                Logger.Instance.Log(ConsoleColor.Yellow, $"Are you sure you want to convert the file at '{fileToConv.FullName}'?\n\ny/n:");
+                string qResp = Console.ReadLine();
+                Logger.Instance.LogToFile(qResp);
+
+                if (!qResp.ToLower().Contains("y"))
+                {
+                    FileInput();
+                    return;
+                }
+            }
+
+            if (fileToConv.Extension.ToLower() == ".png" || fileToConv.Extension.ToLower() == ".jpg" || fileToConv.Extension.ToLower() == ".bmp")
+            {
+                ProcessImage();
+                return;
+            }
+
+            Logger.Instance.Log($"File Size: {fileToConv.Length}");
+
+            byte[] byteArr = File.ReadAllBytes(fileToConv.FullName);
+
+            double lenSqrt = Math.Sqrt(byteArr.Length);
+            Logger.Instance.Log(ConsoleColor.Green, $"Square root of Byte Array ({byteArr.Length}): {lenSqrt}");
+            int dimen = (int)Math.Ceiling(lenSqrt);
+            Logger.Instance.Log(ConsoleColor.Green, $"Rounded up square root of Byte Array ({lenSqrt}): {dimen}");
+
+            Bitmap bMP = new(dimen, dimen, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+
+            Logger.Instance.Log(ConsoleColor.Blue, $"Converting {fileToConv.Name} to an image...");
+
+            for (int y = 0; y < dimen; ++y)
+            {
+                for (int x = 0; x < dimen; ++x)
+                {
+                    int index = (byteArr.Length - 1) - ((byteArr.Length - 1) - (x + y * dimen));
+                    if (index > byteArr.Length - 1)
+                    {
+                        bMP.SetPixel(x, y, Color.FromArgb(255, 255, 255, 255));
+                    }
+                    else
+                    {
+                        Color col = Color.FromArgb(255, byteArr[index], 0, 0);
+                        //Logger.Instance.Log(col.R.ToString());
+                        bMP.SetPixel(x, y, col);
+                    }
+                }
+            }
+
+            //for (int i = 0; i < byteArr.Length; ++i)
+            //{
+            //    Logger.Instance.Log(bMP.GetPixel(i, 0).ToString());
+            //}
+
+            bMP.Save($"{fileToConv.Name}.png", ImageFormat.Png);
+
+            Logger.Instance.Log(ConsoleColor.Green, $"Saved {fileToConv.Name}.png!");
+
+            Console.ReadKey(false);
+        }
+
+        /// <summary>
+        /// Get user's input for the file path
+        /// </summary>
+        private static void FileInput()
+        {
+            Logger.Instance.Log("File Path:");
+            string filePath = "[EMPTY]";
+            OpenFileDialog openFileDialog = new()
+            {
+                Filter = "Image File (*.png;*.jpg;*.bmp)|*.png;*.jpg;*.bmp|All Files (*.*)|*.*",
+                FilterIndex = 2,
+                InitialDirectory = Environment.CurrentDirectory,
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) filePath = openFileDialog.FileName;
+
+            filePath = string.IsNullOrWhiteSpace(filePath) ? "[EMPTY]" : filePath;
+
+            Logger.Instance.LogToFile(filePath);
+
+            fileToConv = new(filePath);
+
+            if (!fileToConv.Exists)
+            {
+                Logger.Instance.Log(ConsoleColor.DarkRed, $"Failed to find file at '{filePath}'. Make sure to include the entire file path.");
+                FileInput();
+                return;
+            }
+
+            Logger.Instance.Log(ConsoleColor.Yellow, $"Are you sure you want to convert the file at '{filePath}'?\n\ny/n:");
+            string qResp = Console.ReadLine();
+            Logger.Instance.LogToFile(qResp);
+
+            if (!qResp.ToLower().Contains("y"))
+            {
+                FileInput();
+                return;
+            }
+
+        }
+
+        private static void ProcessImage()
+        {
+            Logger.Instance.Log(ConsoleColor.Blue, $"Converting {fileToConv.Name} to a file...");
+
+            byte[] data = new byte[] { };
+
+            Bitmap bMP = new(fileToConv.FullName);
+            for (int y = 0; y < bMP.Height; ++y)
+            {
+                for (int x = 0; x < bMP.Width; ++x)
+                {
+                    Color pixelData = bMP.GetPixel(x, y);
+                    if (pixelData != Color.FromArgb(255, 255, 255, 255))
+                    {
+                        byte newByteData = pixelData.R;
+                        List<byte> byteList = data.ToList();
+                        byteList.Add(newByteData);
+                        data = byteList.ToArray();
+                        //Logger.Instance.Log(newByteData.ToString());
+                    }
+                }
+            }
+
+            bMP.Dispose();
+
+            File.WriteAllBytes("EntityWorld.exe", data);
+
+            Logger.Instance.Log(ConsoleColor.Green, $"Done!");
+
+        }
+
+    }
+}
